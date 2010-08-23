a85db8789df22ecdc92bcc0fe4453d8bccf0cdea
Ivan A-R <ivan+nb@alferov.name>
2010-08-24 03:16:40 +0400

    move display routines

--- apps/common/common.mk
+++ apps/common/common.mk
@@ -1,5 +1,7 @@
-COMMONINC = $(COMMON)
+COMMONINC = $(COMMON) $(COMMON)/ui
 COMMONSRC = \
+	   $(COMMON)/ui/display.c \
+	   $(COMMON)/ui/display_hw_routine.c \
 	   $(COMMON)/rtc.c \
 	   $(COMMON)/utils.c
 
